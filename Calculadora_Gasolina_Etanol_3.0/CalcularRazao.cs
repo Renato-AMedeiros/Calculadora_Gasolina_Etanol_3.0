021030ba5b97f52132041e6746e2ecec6445a270
Renato Afonso de Medeiros <renato_ads@outlook.com>
2023-01-23 07:47:02 -0300

    Adicionar arquivos de projeto.

---
+++ Calculadora_Gasolina_Etanol_3.0/CalcularRazao.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calculadora_Gasolina_Etanol_3._0
+{
+    class CalcularRazao
+    {
+        private double _combustivelA;
+        public double CombustivelA
+        {
+            get { return _combustivelA; }
+            set { _combustivelA = value; }
+        }
+
+
+        private double _combustivelB;
+        public double CombustivelB
+        {
+            get { return _combustivelB; }
+            set { _combustivelB = value; }
+        }
+        
+
+        public double Razao
+        {
+            get
+            {
+                return CombustivelB / CombustivelA;
+            }
+
+            set
+            {
+                Razao = value;    
+            }          
+         }
+
+        public double va;
+
+        public double VA
+        {
+            get { return va; }
+            set { va = value; }
+        }
+
+
+        public double economiaG; // Economia Gasolina
+
+        public double EconomiaG
+        {
+            get { return (VA * (Razao - 0.73)); }
+            set { economiaG = value; }
+        }
+       
+        public double economiaE; // Economia Etanol
+
+        public double EconomiaE
+        {
+            get { return (VA * (0.73 - Razao)); }
+            set { economiaE = value; }
+        }
+
+
+        public void exibirDados()
+        {
+            double ExibirDados = Razao;
+
+            if (Razao >= 0.73)//Gasolina
+            {
+                Console.WriteLine($"\nO combustivel mais vantajoso é a Gasolina.");
+                
+                Console.WriteLine($"\nVocê está economizando R${EconomiaG:f2} ");
+            }
+
+            else // Etanol
+            {
+                Console.WriteLine($"\nO combustivel mais vantajoso é o Etanol.");
+                Console.WriteLine($"\nVocê está economizando R${EconomiaE:f2} ");
+            }
+
+
+        }
+    }
+}
---
+++ Calculadora_Gasolina_Etanol_3.0/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Calculadora_Gasolina_Etanol_3._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CalcularRazao c1 = new CalcularRazao();
+
+            double ValorGasolina, ValorEtanol, ValorAbastecimento, Economia;
+
+            Console.WriteLine("Calculadora Gasolina x Etanol.");
+
+            Console.Write("\nInsira o preço atual da gasolina: R$");
+            c1.CombustivelA = ValorGasolina = double.Parse(Console.ReadLine());
+            while (ValorGasolina < 0)
+            {
+                Console.Write("ERRO! \nVocê digitou um número negativo. Insira o preço atual da gasolina válido: ");
+                ValorGasolina = double.Parse(Console.ReadLine());
+
+
+            }
+
+            Console.Write("\nInsira o preço atual do etanol: R$");
+            c1.CombustivelB = ValorEtanol = double.Parse(Console.ReadLine());
+            while (ValorEtanol < 0)
+            {
+                Console.Write("ERRO! \nVocê digitou um número negativo. Insira o preço atual do etanol válido: ");
+                ValorEtanol = double.Parse(Console.ReadLine());
+
+            }
+
+            Console.Write("\nInsira o valor a abastecer: R$");
+           c1.VA = ValorAbastecimento = double.Parse(Console.ReadLine());
+            while (ValorAbastecimento < 0)
+            {
+                Console.Write("ERRO! \nVocê digitou um número negativo. Insira um valor a abastecer válido: ");
+                ValorAbastecimento = double.Parse(Console.ReadLine());
+
+            }
+
+            c1.exibirDados();
+
+            Console.Write("\nAperte a tecla ENTER para sair.");
+            Console.ReadKey();
+        }
+    }
+}
