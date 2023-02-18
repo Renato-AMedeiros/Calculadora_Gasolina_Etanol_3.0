8d72aff7daf92984a5d30bbd3507cb4238fdecda
Renato Afonso de Medeiros <renato_ads@outlook.com>
2023-02-18 18:52:47 -0300

    calcular ecnomia

---
+++ CGE.Api/Controllers/EconomiaController.cs
@@ -0,0 +1,41 @@
+﻿using Calculadora_Gasolina_Etanol_3._0;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CGE.Api.Controllers
+{
+
+    [ApiController]
+    [Route("[controller]")]
+    public class EconomiaController : ControllerBase
+    {
+        [HttpGet("CalcularEconomia")]
+        public string CalcularEconomia(double EconomiaGasolina, double EconomiaEtanol)
+        {
+            var CombustivelMaisEconomico = new CalcularRazao();
+           
+            CombustivelMaisEconomico.EconomiaG = EconomiaGasolina;
+
+            CombustivelMaisEconomico.EconomiaE = EconomiaEtanol;
+           
+           
+
+            if (EconomiaGasolina >= EconomiaEtanol)
+            {
+                return " Economia de: " + EconomiaGasolina;
+
+            }
+           else 
+            {
+                return "Economia de: " + EconomiaEtanol;
+            }
+
+
+
+    }
+
+
+
+
+
+    }
+}
